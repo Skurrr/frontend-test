b56aa87ae476e6f139eddf2b47537754e55290e9
tpabj <tpabjanczyk@gmail.com>
2022-03-27 18:44:31 +0200

    feature: remove navLink, add button to navigate to users page

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,15 +1,23 @@
-import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import { Link, useNavigate } from "react-router-dom";
 import "./Home.css";
 
 const Home = () => {
+  let navigate = useNavigate();
+
   return (
     <div className="Home">
       <header className="Home-header">
         <p>Welcome</p>
+        <Button
+          onClick={() => {
+            navigate(`/users`);
+          }}
+          variant="text"
+        >
+          Go to users page
+        </Button>
       </header>
-      <nav>
-        <Link to="/users">Users</Link> |{" "}
-      </nav>
     </div>
   );
 };
